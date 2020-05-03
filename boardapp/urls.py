cee83b4e5e98d9457b8e5ad96e5bd7f259eca85a
uuta <yuta.morishita0916gmail.com>
2020-05-03 23:37:56 +0900

    signup login commit

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signupfunc),
+    path('signup/', views.signupfunc, name='signup'),
+    path('login/', views.loginfunc, name='login'),
 ]
